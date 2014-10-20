3de12a118b4ce9005fd5f7d54822233f2673eec3
Eugene Ware <eugene@noblesamurai.com>
2014-10-20 23:01:17 +1100

    split content

--- test/edge.js
+++ test/edge.js
@@ -114,3 +114,38 @@ it('should be able to parse content', function(t) {
     w.write(buf);
   });
 });
+
+it('should be able to parse split content', function(t) {
+  t.plan(3);
+  var w = new WARCStream();
+  w.on('protocol', function (protocol) {
+    t.equal(protocol, 'WARC/1.0');
+  })
+  w.on('headers', function (headers) {
+    var expected = {
+      'Content-Type': 'application/warc-fields',
+      'Content-Length': '108' };
+    t.deepEquals(headers, expected);
+  });
+  w.on('content', function (content) {
+    var expected = [
+      'Software-Info: ia-web-commons.1.0-SNAPSHOT-20140819100050\r\n',
+      'Extracted-Date: Tue, 09 Sep 2014 10:14:59 GMT\r\n',
+      '\r\n'].join('');
+    t.equals(content.toString(), expected);
+  });
+  var chunks = [
+    'WARC/1.0\r\n',
+    'Content-Type: ', 'application/warc-fields\r\n',
+    'Content-Len', 'gth: 108', '\r\n',
+    '\r\n',
+    'Software-Info:', ' ia-web-commons.1.0-SNAPSHOT-20140819100050\r\n',
+    'Extracted-Date: Tue, ', '09 Sep 2014 10:14:59 GMT', '\r\n',
+    '\r\n'
+  ];
+
+  chunks.forEach(function (chunk) {
+    var buf = new Buffer(chunk);
+    w.write(buf);
+  });
+});
